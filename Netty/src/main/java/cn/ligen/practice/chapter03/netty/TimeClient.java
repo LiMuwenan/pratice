4f32d43ebfdd423a761d1e71c5fb1790cb876be4
ligen <bear.gen.li@outlook.com>
2024-04-06 18:24:28 +0800

    netty server、client

---
+++ Netty/src/main/java/cn/ligen/practice/chapter03/netty/TimeClient.java
@@ -0,0 +1,53 @@
+package cn.ligen.practice.chapter03.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author: ligen
+ * @date: 2024/4/2 22:54
+ * @description:
+ */
+public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("port converter error");
+            }
+        }
+
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+
+    public void connect(int port, String host) {
+        // 配置客户端NIO线程组
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+            // 发起异步连接
+            ChannelFuture sync = bootstrap.connect(host, port).sync();
+
+            // 等待客户端链路关闭
+            sync.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
